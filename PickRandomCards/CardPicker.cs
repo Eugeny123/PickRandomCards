03033ce6b24ccfa1bf2cf41d79d582a68a01b03b
Eugeny <sgenya72@gmail.com>
2022-09-30 15:27:31 +0300

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickerCards = new string[numberOfCards];
+
+            for (int i=0; i < numberOfCards; i++)
+            {
+                pickerCards[i] = RandomValue() + "of" + RandomSuit();
+            }
+            return pickerCards;
+        }
+
+        private static string RandomSuit()
+        {
+            return;
+        }
+
+        private static string RandomValue()
+        {
+            return;
+        }
+    }
+}
